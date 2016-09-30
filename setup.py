e2d2d560cbca47cecd199fedf177efe72dd0a26f
leovt <leonhard.vogt@gmx.ch>
2016-09-30 13:34:25 +0200

    Release 0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name='constructible',
   py_modules=['constructible'],  # this must be the same as the name above
-  version='0.1',
+  version='0.2',
   description='Exact Constructible Numbers Representation',
   author='Leonhard Vogt',
   author_email='leonhard.vogt@gmx.ch',
   url='https://github.com/leovt/constructible',  # use the URL to the github repo
-  download_url='https://github.com/leovt/constructible/tarball/0.1',
+  download_url='https://github.com/leovt/constructible/tarball/0.2',
   keywords=[],  # arbitrary keywords
   classifiers=[],
 )
