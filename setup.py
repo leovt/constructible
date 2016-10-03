7b3ba65a4a179a421eacbfd1c497a281066afd9a
leovt <leonhard.vogt@gmx.ch>
2016-10-03 09:52:25 +0200

    setup for proper README on PyPi

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 setup(
   name='constructible',
   py_modules=['constructible'],  # this must be the same as the name above
-  version='0.2',
+  version='0.3.1',
   description='Exact Constructible Numbers Representation',
+  long_description=open('README.rst', 'rt').read(),
   author='Leonhard Vogt',
   author_email='leonhard.vogt@gmx.ch',
   url='https://github.com/leovt/constructible',  # use the URL to the github repo
-  download_url='https://github.com/leovt/constructible/tarball/0.2',
+  download_url='https://github.com/leovt/constructible/tarball/0.3.1',
   keywords=[],  # arbitrary keywords
   classifiers=[],
 )
